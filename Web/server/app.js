0e7c95c1dc16c4dfd8c8a757758e43a0839c2892
Patrick Rotter <patrick.rotter@gmail.com>
2022-05-22 13:48:42 +0200

    Added demo-challenges and added GET-Request to request a random challenge from demo challenges

--- Web/server/app.js
+++ Web/server/app.js
@@ -1,9 +1,43 @@
-const express = require("express");
+import express from "express";
+import challenges from "./public/challenges.js";
+
 const app = express();
 const port = 5000;
+let randomNumber = 0;
+/* Initialize previous number with array.length + 1 
+to make sure every challenge can occur at the start */
+let previousNumber = challenges.length + 1;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Generate a random number (but different from previous random nubmer)
+const generateUniqueRandomNum = () => {
+  let isPreviousNumber = true;
+
+  while (isPreviousNumber) {
+    randomNumber = Math.floor(Math.random() * challenges.length);
+    if (randomNumber !== previousNumber) {
+      isPreviousNumber = false;
+    }
+  }
+
+  previousNumber = randomNumber;
+  return randomNumber;
+};
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// GET a random challenge - prevents getting the same challenge twice
+app.get(`/api/challenges`, async (req, res) => {
+  return res.status(200).send(challenges[generateUniqueRandomNum()]);
 });
 
 app.listen(port, () => {
---
+++ Web/server/public/challenges.js
@@ -0,0 +1,26 @@
+const distanceClick = "Distance Click";
+const joystickClick = "Joystick Click";
+const touchClick = "Touch Click";
+
+const challenges = [
+  {
+    title: "Measure 7 cm",
+    module: distanceClick,
+    img: "https://merriam-webster.com/assets/mw/images/gallery/gal-wap-slideshow-slide/image1294857992-5173-f5c77f4215a3f8bec22ab164b6e07450@1x.jpg",
+    description: "Measure a distance of 7 cm and push the button!",
+  },
+  {
+    title: "Push Up",
+    module: joystickClick,
+    img: "https://asset.conrad.com/media10/isa/160267/c1/-/de/1893215_RB_01_FB/speedlink-competition-pro-extra-joystick-usb-pc-android-schwarz-rot.jpg",
+    description: "Push up the Joystick!",
+  },
+  {
+    title: "Touch the lower touch button",
+    module: touchClick,
+    img: "https://i.ytimg.com/vi/ba2RsswFNpg/maxresdefault.jpg",
+    description: "Touch the lower touch button on the touchpad!",
+  },
+];
+
+export default challenges;
