1cefa0fb2a9c9010395e961bc85c70abb9de2882
Patrick Rotter <patrick.rotter@gmail.com>
2022-05-27 14:04:53 +0200

    Added header and footer

--- Web/client/src/components/App.jsx
+++ Web/client/src/components/App.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React from "react";
 import ChallengeCard from "./ChallengeCard";
+import Header from "./Header";
+import Footer from "./Footer"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ChallengeCard />
+      <Footer />
     </div>
   );
 }
---
+++ Web/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Made with love in 2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
