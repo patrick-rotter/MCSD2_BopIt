4300a6aa65bbff3d5b7477e896d8b5a24b71e47b
Patrick Rotter <patrick.rotter@gmail.com>
2022-05-27 12:30:07 +0200

    Added more challenges (total 3 per MCU for a total of 9)

--- Web/server/public/challenges.js
+++ Web/server/public/challenges.js
@@ -10,16 +10,52 @@ const challenges = [
     description: "Measure a distance of 7 cm and push the button!",
   },
   {
+    title: "Measure 11 cm",
+    module: distanceClick,
+    img: "https://merriam-webster.com/assets/mw/images/gallery/gal-wap-slideshow-slide/image1294857992-5173-f5c77f4215a3f8bec22ab164b6e07450@1x.jpg",
+    description: "Measure a distance of 11 cm and push the button!",
+  },
+  {
+    title: "Measure 23 cm",
+    module: distanceClick,
+    img: "https://merriam-webster.com/assets/mw/images/gallery/gal-wap-slideshow-slide/image1294857992-5173-f5c77f4215a3f8bec22ab164b6e07450@1x.jpg",
+    description: "Measure a distance of 23 cm and push the button!",
+  },
+  {
     title: "Push Up",
     module: joystickClick,
     img: "https://asset.conrad.com/media10/isa/160267/c1/-/de/1893215_RB_01_FB/speedlink-competition-pro-extra-joystick-usb-pc-android-schwarz-rot.jpg",
-    description: "Push up the Joystick!",
+    description: "Move the Joystick up!",
+  },
+  {
+    title: "Push Left",
+    module: joystickClick,
+    img: "https://asset.conrad.com/media10/isa/160267/c1/-/de/1893215_RB_01_FB/speedlink-competition-pro-extra-joystick-usb-pc-android-schwarz-rot.jpg",
+    description: "Move the Joystick left!",
+  },
+  {
+    title: "Push Down",
+    module: joystickClick,
+    img: "https://asset.conrad.com/media10/isa/160267/c1/-/de/1893215_RB_01_FB/speedlink-competition-pro-extra-joystick-usb-pc-android-schwarz-rot.jpg",
+    description: "Move the Joystick right!",
   },
   {
     title: "Touch the lower touch button",
     module: touchClick,
     img: "https://i.ytimg.com/vi/ba2RsswFNpg/maxresdefault.jpg",
     description: "Touch the lower touch button on the touchpad!",
+  },
+  {
+    title: "Touch the upper touch button",
+    module: touchClick,
+    img: "https://i.ytimg.com/vi/ba2RsswFNpg/maxresdefault.jpg",
+    description: "Touch the upper touch button on the touchpad!",
+  },
+  {
+    title: "Slide your finger",
+    module: touchClick,
+    img: "https://i.ytimg.com/vi/ba2RsswFNpg/maxresdefault.jpg",
+    description: "Slide your finger across the touchpad!",
   },
 ];
 
